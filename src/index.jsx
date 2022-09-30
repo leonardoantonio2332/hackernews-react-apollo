1eac40d47fb067a1aaa4eec33ae023cb94b94d4f
Leonardo Teixeira <leonardo.teixeira@acad.ftec.com.br>
2022-09-30 17:29:37 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../src/styles/index.css';
+import App from './components/App';
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+  } from '@apollo/client';
+
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
